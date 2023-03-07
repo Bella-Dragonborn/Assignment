37edec173d34191ec620397629449fd554ae2659
Bella-Dragonborn <126836282+Bella-Dragonborn@users.noreply.github.com>
2023-03-07 23:28:02 +0000

    Add files via upload

---
+++ Assignment/Assignment/Card.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+    class Card
+    {
+        //Base for the Card class.
+        //Value: numbers 1 - 13
+        //Suit: numbers 1 - 4
+        //The 'set' methods for these properties could have some validation
+        public int Value { get; set; }
+        public int Suit { get; set; }
+        public Card(int suit2, int cardvalue2)
+        {
+            Suit = suit2;
+            Value = cardvalue2;
+        }
+        public Card[] Cards
+        {
+            get
+            {
+                return Cards;
+
+            }
+        }
+        public string GetCard
+        {
+            get{
+                return Value + " " + Suit;
+            }
+        }
+    }
+}
---
+++ Assignment/Assignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+
+            Pack myPack = new Pack();
+            myPack.PrintPack();
+            Console.WriteLine(" [1] - Fisher-Yates Shuffle, " +
+                "[2] - Riffle Shuffle, [3] - No Shuffle");
+
+            Pack.shuffleCardPack(1);
+            myPack.PrintPack();
+            Console.WriteLine("");
+            Pack.shuffleCardPack(2);
+            myPack.PrintPack();
+            Console.WriteLine("");
+            Pack.shuffleCardPack(3);
+            
+            Console.WriteLine("\nDeal card:\n");
+            Console.WriteLine(Pack.dealCard().GetCard);
+
+            Console.WriteLine("\n\n");
+            myPack.PrintPack();
+
+
+            Console.ReadLine();
+        }
+    }
+}
